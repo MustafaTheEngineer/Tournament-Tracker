ba7dea9ff820044de20e443e938a60f9eca822cd
MustafaTheEngineer <mustafaesat01@gmail.com>
2022-08-19 20:45:08 +0300

    Commit 8 - Working on connection to text based database.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string prizesFile = "PrizeModel.csv";
         // TODO - Wire up the createPrize for text files
         public PrizeModel createPrize(PrizeModel model)
         {
-            model.Id = 1;
+            // * Load the text file
+            // * Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = prizesFile.fullFilePath().loadFile().convertToPrizeModels();
 
-            return model;
+            // Find the max ID
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+
+            
+            
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file.
         }
     }
 }
